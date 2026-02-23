a5353db516919a1ea5a6392be247ec6bbe9a85ac
Suraj pandey <surajpandey7639b@gmail.com>
2026-02-23 19:29:03 +0530

    Added practical-3 Set A programs

--- FullPyramid.java
+++ FullPyramid.java
@@ -1,13 +1,14 @@
 public class FullPyramid {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) {     // Rows
 
-
+            // Print spaces
             for (int space = 1; space <= 5 - i; space++) {
                 System.out.print(" ");
             }
 
+            // Print stars
             for (int star = 1; star <= i; star++) {
                 System.out.print("* ");
             }
